fb7f6f93927b8ee7fb058a02a4824cf327b1e009
Vanessapham23 <Ladyy_vanessa@yahoo.com>
2023-07-17 19:31:39 -0600

    committed

---
+++ src/test/java/variousConcepts/GitTest2.java
@@ -0,0 +1,19 @@
+package variousConcepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("Git test2");
+		
+	}
+	
+	
+	public static void sndtest(){
+		System.out.println("Git test2");
+		System.out.println("Git test3");
+		System.out.println("Git test4");
+		System.out.println("Git test5");
+		System.out.println("Git test6");
+	}
+
+}
